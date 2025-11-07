edf98203174dc432ef20eafd70668fe81a5ee268
DarkNaku <skyrack@naver.com>
2025-11-07 12:06:01 +0900

    유틸리티 추가

---
+++ Assets/FoundationDI/Runtime/Utilities/SafeAreaFitter.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaFitter : MonoBehaviour {
+    [SerializeField] private RectTransform _rectTransform;
+
+    private Rect _prevSafeArea;
+    private Vector3[] _corners = new Vector3[4];
+
+    private void OnValidate() {
+        _rectTransform ??= GetComponent<RectTransform>();
+    }
+
+    private void LateUpdate() {
+        FitToSafeArea();
+    }
+
+    public void FitToSafeArea() {
+        if (_rectTransform == null) return;
+        if (Screen.safeArea == _prevSafeArea) return;
+
+        Rect safeArea = Screen.safeArea;
+
+        Vector2 anchorMin = safeArea.min;
+        Vector2 anchorMax = safeArea.min + safeArea.size;
+
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        _rectTransform.anchorMin = anchorMin;
+        _rectTransform.anchorMax = anchorMax;
+
+        _prevSafeArea = safeArea;
+    }
+}
---
+++ Assets/FoundationDI/Runtime/Utilities/SpriteStretcher.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteStretcher : MonoBehaviour {
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Camera targetCamera;
+    [SerializeField] private bool maintainAspectRatio = true;
+
+    private float prevAspect = 0f;
+
+    private void OnValidate() {
+        spriteRenderer ??= GetComponent<SpriteRenderer>();
+    }
+
+    private void LateUpdate() {
+        Stretch();
+    }
+
+    private void Stretch() {
+        if (spriteRenderer == null) return;
+        if (targetCamera == null) return;
+        if (Mathf.Approximately(targetCamera.aspect, prevAspect)) return;
+
+        transform.localScale = Vector3.one;
+
+        var sprite = spriteRenderer.sprite;
+        var pixelsPerUnit = sprite.pixelsPerUnit;
+        var unitWidth = sprite.texture.width / pixelsPerUnit;
+        var unitHeight = sprite.texture.height / pixelsPerUnit;
+        var screenHeight = targetCamera.orthographicSize * 2f;
+        var screenWidth = screenHeight * targetCamera.aspect;
+        var scaleX = screenWidth / unitWidth;
+        var scaleY = screenHeight / unitHeight;
+
+		switch (spriteRenderer.drawMode) {
+            case SpriteDrawMode.Sliced:
+            case SpriteDrawMode.Tiled:
+                spriteRenderer.size = new Vector2(screenWidth, screenHeight);
+                break;
+            default:
+                if (maintainAspectRatio) {
+                    var scale = Mathf.Max(scaleX, scaleY);
+                    transform.localScale = new Vector3(scale, scale, 1F);
+                } else {
+                    transform.localScale = new Vector3(scaleX, scaleY, 1F);
+                }
+                break;
+        }
+
+        prevAspect = targetCamera.aspect;
+    }
+}
