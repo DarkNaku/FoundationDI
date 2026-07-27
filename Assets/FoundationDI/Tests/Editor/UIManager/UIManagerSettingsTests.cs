b8b4412fd8fb5dd58062206df910138ecdfd7b47
DarkNaku <skyrack@naver.com>
2026-07-27 12:11:39 +0900

    [BEHAVIORAL] UIManagerSettings에 SortingLayerName/SortingOrder/PlaneDistance 추가

---
+++ Assets/FoundationDI/Tests/Editor/UIManager/UIManagerSettingsTests.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+using DarkNaku.FoundationDI;
+
+public class UIManagerSettingsTests
+{
+    [Test]
+    public void UIManagerSettings는_정렬레이어_정렬순서_평면거리를_설정값으로_반환한다()
+    {
+        var settings = ScriptableObject.CreateInstance<UIManagerSettings>();
+        var so = new SerializedObject(settings);
+        so.FindProperty("_sortingLayerName").stringValue = "UI";
+        so.FindProperty("_sortingOrder").intValue = 5;
+        so.FindProperty("_planeDistance").floatValue = 42f;
+        so.ApplyModifiedPropertiesWithoutUndo();
+
+        Assert.AreEqual("UI", settings.SortingLayerName);
+        Assert.AreEqual(5, settings.SortingOrder);
+        Assert.AreEqual(42f, settings.PlaneDistance);
+
+        Object.DestroyImmediate(settings);
+    }
+}
