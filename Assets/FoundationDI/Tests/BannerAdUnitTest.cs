9b83586c18b4d74727c7053a8432fb3ac22803c7
DarkNaku <skyrack@naver.com>
2026-08-20 17:11:10 +0900

    [BEHAVIORAL] 배너 숨김/파괴/재부착과 임프레션 중계 검증 추가

--- Assets/FoundationDI/Tests/BannerAdUnitTest.cs
+++ Assets/FoundationDI/Tests/BannerAdUnitTest.cs
@@ -38,4 +38,65 @@ public class BannerAdUnitTest
         Assert.AreEqual(120f, sut.Height, 0.001f);
         Assert.AreEqual(120f, reported, 0.001f, "HeightChanged가 중계되지 않았다");
     }
+
+    [Test]
+    public void 배너를_숨기면_어댑터를_유지한_채_높이를_0으로_보고한다()
+    {
+        var factory = new AdapterFactory();
+        var sut = new BannerAdUnit(factory.Create, () => false);
+
+        sut.Show();
+        factory.Last.SetHeight(120f);
+
+        var reported = -1f;
+        sut.HeightChanged += h => reported = h;
+        sut.Hide();
+
+        Assert.IsFalse(sut.IsVisible);
+        Assert.AreEqual(0f, sut.Height, 0.001f);
+        Assert.AreEqual(0f, reported, 0.001f);
+        Assert.AreEqual(1, factory.Last.HideCount);
+        Assert.IsFalse(factory.Last.IsDisposed, "Hide가 어댑터를 파괴했다");
+        Assert.AreEqual(1, factory.Created.Count);
+    }
+
+    [Test]
+    public void 배너를_파괴하면_어댑터를_해제하고_다음_표시에서_새로_만든다()
+    {
+        var factory = new AdapterFactory();
+        var sut = new BannerAdUnit(factory.Create, () => false);
+
+        sut.Show();
+        var first = factory.Last;
+
+        sut.Destroy();
+
+        Assert.IsTrue(first.IsDisposed, "어댑터가 해제되지 않았다");
+        Assert.AreEqual(0f, sut.Height, 0.001f);
+
+        sut.Show();
+
+        Assert.AreEqual(2, factory.Created.Count, "파괴 후 어댑터를 새로 만들지 않았다");
+        Assert.AreNotSame(first, factory.Last);
+        Assert.AreEqual(1, factory.Last.ShowCount);
+        Assert.IsTrue(sut.IsVisible);
+    }
+
+    [Test]
+    public void 배너_임프레션은_그대로_중계된다()
+    {
+        var factory = new AdapterFactory();
+        var sut = new BannerAdUnit(factory.Create, () => false);
+        AdImpression? received = null;
+        sut.Paid += imp => received = imp;
+
+        sut.Show();
+        factory.Last.RaisePaid(new AdImpression(AdFormat.Banner, "Dummy", "TestNetwork", "banner-unit",
+                                                "inst", null, 0.004, "USD",
+                                                AdRevenuePrecision.Estimated, "creative-1"));
+
+        Assert.IsTrue(received.HasValue, "임프레션이 중계되지 않았다");
+        Assert.AreEqual("TestNetwork", received.Value.NetworkName);
+        Assert.AreEqual(0.004, received.Value.Revenue, 0.0001);
+    }
 }
