f0812e24b61d44345488cbb30aa7b0f0617b3f84
DarkNaku <skyrack@naver.com>
2026-08-21 15:15:02 +0900

    [STRUCTURAL] UIRoot 기본 구성 특성화 테스트 복원
    Task 1에서 생성자와 함께 사라진 캔버스/스케일러/상주화 단언을
    CreateDefault() 기준으로 되살린다. 프로덕션 코드 변경 없음.

--- Assets/FoundationDI/Tests/Runtime/UIService/UIRootTests.cs
+++ Assets/FoundationDI/Tests/Runtime/UIService/UIRootTests.cs
@@ -17,4 +17,43 @@ public class UIRootTests
 
         Object.DestroyImmediate(root.GO);
     }
+
+    [Test]
+    public void CreateDefault의_레이어는_sibling_순서가_렌더_순서와_같다()
+    {
+        var root = UIRoot.CreateDefault();
+
+        Assert.AreEqual(0, root.PageLayer.GetSiblingIndex());
+        Assert.AreEqual(1, root.BelowOverlayLayer.GetSiblingIndex());
+        Assert.AreEqual(2, root.PopupLayer.GetSiblingIndex());
+        Assert.AreEqual(3, root.AboveOverlayLayer.GetSiblingIndex());
+
+        Object.DestroyImmediate(root.GO);
+    }
+
+    [Test]
+    public void CreateDefault는_ScreenSpaceOverlay와_ScaleWithScreenSize_Expand로_구성한다()
+    {
+        var root = UIRoot.CreateDefault();
+        var canvas = root.GO.GetComponent<Canvas>();
+        var scaler = root.GO.GetComponent<CanvasScaler>();
+
+        Assert.AreEqual(RenderMode.ScreenSpaceOverlay, canvas.renderMode);
+        Assert.AreEqual(CanvasScaler.ScaleMode.ScaleWithScreenSize, scaler.uiScaleMode);
+        Assert.AreEqual(CanvasScaler.ScreenMatchMode.Expand, scaler.screenMatchMode);
+        Assert.AreEqual(UIRoot.DefaultReferenceResolution, scaler.referenceResolution);
+
+        Object.DestroyImmediate(root.GO);
+    }
+
+    [Test]
+    public void CreateDefault는_DontDestroyOnLoad를_적용하지_않는다()
+    {
+        var root = UIRoot.CreateDefault();
+
+        Assert.AreNotEqual("DontDestroyOnLoad", root.GO.scene.name,
+            "상주화는 UIService의 책임이다. 에디터 프리팹 조립에도 쓰이므로 여기서 하면 안 된다.");
+
+        Object.DestroyImmediate(root.GO);
+    }
 }
